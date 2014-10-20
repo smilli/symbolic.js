500b0ebb3e9bc7b52ef8f6bc661a9bf05f0a1cd3
Smitha Milli <smitha.milli@gmail.com>
2014-10-20 14:47:18 -0700

    Add jsdocs

--- src/expr.js
+++ src/expr.js
@@ -1,6 +1,25 @@
+/**
+ * A node in an expression tree.  Can be an operation, symbol, or number.
+ * @constructor
+ * @param {(number|Symbol|string)} value - The value of this expression node
+ * @param {Expr[]} [operands] - The children of this expression node.  A node
+ *  should only have operands if the value is an operator.
+ */
 function Expr(value, operands) {
+  this.value = value;
+  if (operands === undefined){
+    this.operands = [];
+  } else {
+    this.operands = operands;
+  }
 }
 
+
+/**
+ * Represents a variable like x or y that should be treated symbolically.
+ * @constructor
+ * @param {string} symbolName - The name of the symbol.  For example 'x' or 'y'.
+ */
 function Symbol(symbolName) {
   this.symbolName = symbolName;
 }
